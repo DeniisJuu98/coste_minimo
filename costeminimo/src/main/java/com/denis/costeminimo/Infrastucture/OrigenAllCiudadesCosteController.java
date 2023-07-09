07e8cf2f666afa0afa04d06e96504afc545a43eb
Denis Iulian Prodisteanu <76124404+DeniisJuu98@users.noreply.github.com>
2023-07-10 00:01:11 +0200

    Update OrigenAllCiudadesCosteController.java

--- costeminimo/src/main/java/com/denis/costeminimo/Infrastucture/OrigenAllCiudadesCosteController.java
+++ costeminimo/src/main/java/com/denis/costeminimo/Infrastucture/OrigenAllCiudadesCosteController.java
@@ -2,10 +2,9 @@ package com.denis.costeminimo.Infrastucture;
 
 import com.denis.costeminimo.Aplication.OrigenAllCiudadesCosteUseCase;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 @RestController
@@ -23,4 +22,14 @@ public class OrigenAllCiudadesCosteController {
     public List<String> getMinCostPath2(@PathVariable String origen) {
         return origenAllCiudadesCosteUseCase.getCosteMinimoDesdeOrigen(origen);
     }
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handleRuntimeException(RuntimeException ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex.getMessage());
+    }
+
 }
