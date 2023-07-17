5f6478036e0701660b93fe9fd202214e1309c318
Denis Iulian Prodisteanu <76124404+DeniisJuu98@users.noreply.github.com>
2023-07-17 17:52:50 +0200

    Update OrigenAllCiudadesCosteController.java

--- costeminimo/src/main/java/com/denis/costeminimo/Infrastucture/OrigenAllCiudadesCosteController.java
+++ costeminimo/src/main/java/com/denis/costeminimo/Infrastucture/OrigenAllCiudadesCosteController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 @RestController
-@RequestMapping("/api/CosteMinimo")
+@RequestMapping("/api/CosteMinimo/OrigenAll")
 public class OrigenAllCiudadesCosteController {
     //inyeccion de datos
     private final OrigenAllCiudadesCosteUseCase origenAllCiudadesCosteUseCase ;
